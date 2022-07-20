90b11a3075da4301bb81aa29ded2bb26b8763fc5
marinacharris <85807129+marinacharris@users.noreply.github.com>
2022-07-19 21:17:36 -0500

    Add files via upload

--- script.sql
+++ script.sql
@@ -104,12 +104,13 @@ select * from medicamentos;
 
 --default
 create table libros(
-	codigo integer primary key,
+	codigo integer primary key, -- clave primaria que se declara con integer se autoincrementa 
 	titulo text,
 	autor text not null default 'desconocido', 
 	editorial text,
 	precio real,
 	cantidad integer default 0
+	--primary key (codigo)descripcion
  );
 
 insert into libros (titulo, precio) values ('Algoritmos',500);
@@ -119,6 +120,133 @@ insert into libros (titulo,autor,editorial,precio,cantidad)
   values('Martin Fierro','Jose Hernandez','Emece',22.20,200);
 
  select * from libros;
+
+-- columnas calculadas
+select * from articulos;
+select descripcion, precio, cantidad, precio*cantidad as VALOR_TOT
+from articulos;
+select descripcion ||'-'||precio||'-'||cantidad as DESCRIPCION_GEN
+from articulos;
+
+-- order by
+select * from articulos order by cantidad;
+select * from articulos order by cantidad desc, descripcion;
+
+insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Antologia poetica','Borges','Planeta',40,150);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Aprenda PHP','Mario Molina','Emece',18.20,200);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Cervantes y el quijote','Borges','Paidos',36.40,100);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Manual de PHP', 'J.C. Paez', 'Paidos',30.80,100);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Harry Potter y la piedra filosofal','J.K. Rowling','Paidos',45.00,500);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Harry Potter y la camara secreta','J.K. Rowling','Paidos',46.00,300);
+ insert into libros (titulo,autor,editorial,precio,cantidad)
+  values('Alicia en el pais de las maravillas','Lewis Carroll','Paidos',null,50);
+
+select * from libros;
+select codigo, titulo, autor, editorial from libros order by editorial, titulo;
+select codigo, titulo from libros order by 2;
+
+--between, rango con límites incluidos
+select *
+from libros
+where precio >= 0 and precio<=30;
+
+select *
+from Libros
+where precio BETWEEN 0 and 30;
+
+--IN
+select *
+from libros
+where autor ='Borges' or autor = 'Mario Molina' or autor ='Jose Hernandez';
+
+select *
+from libros
+where autor in ('Borges', 'Mario Molina','Jose Hernandez');
+
+select *
+from libros
+where autor not in('Borges', 'Mario Molina', 'Jose Hernandez');
+
+--is null, is not null
+select * from libros where editorial is null;
+select * from libros where editorial is not null;
+
+--like y not like, compara cadenas o subcadenas
+select * from libros where titulo like '%Potter%';
+select * from libros where autor like 'B%';
+
+--funciones agregadas: count, sum, max, min, avg
+select * from libros;
+select count(*) from libros;
+select count(editorial) from libros; -- count no cuenta los valores null
+select sum(cantidad) from libros;
+select count(*) from libros where editorial = 'Planeta';
+
+--group by
+
+select editorial, count(*)
+from libros
+group by editorial;
+
+select autor, count(*)
+from libros
+group by autor;
+
+-- having, es como un where perose puede aplicar a las funciones agregadas
+select autor, count(*)
+from libros
+group by autor 
+having COUNT(*)>1; 
+
+--join
+drop table libros;
+create table libros(
+	codigo integer primary key,
+	titulo text,
+	autor text, 
+	precio real,
+	codigoeditorial integer,
+	foreign key (codigoeditorial) references editoriales(codigo)
+);
+create table editoriales(
+	codigo integer primary key,
+	nombre text
+);
+insert into editoriales(nombre) values('Planeta');
+ insert into editoriales(nombre) values('Emece');
+ insert into editoriales(nombre) values('Siglo XXI');
+
+insert into libros (titulo, autor, codigoeditorial, precio)
+  values('El aleph', 'Borges', 2, 34);
+ insert into libros (titulo, autor, codigoeditorial, precio)
+  values('Antología poética', 'Borges', 1, 39.50);
+ insert into libros (titulo, autor, codigoeditorial, precio)
+  values('Java en 10 minutos', 'Mario Molina', 1, 50.50);
+ insert into libros (titulo, autor, codigoeditorial, precio)
+  values('Alicia en el pais de las maravillas', 'Lewis Carroll', 2, 19.90);
+ insert into libros (titulo, autor, codigoeditorial, precio)
+  values('Martin Fierro', 'Jose Hernandez', 2, 25.90);
+ insert into libros (titulo, autor, codigoeditorial, precio)
+  values('Martin Fierro', 'Jose Hernandez', 3, 16.80);
+ 
+
+--JOIN
+select *
+from libros 
+join editoriales on (libros.codigoeditorial=editoriales.codigo);
+
+select l.codigo, titulo, autor, nombre
+from libros l 
+join editoriales e  on (l.codigoeditorial = e.codigo);
+
+
+
 
 
 
